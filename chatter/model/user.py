0386d018d1f2c4c37abf1577d3a1184e84b9fa62
Mehrzad Asri <mehrzad.asri@protonmail.ch>
2023-01-29 21:29:20 +0330

    Add validation for user fields

--- chatter/model/user.py
+++ chatter/model/user.py
@@ -1,13 +1,43 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, validator
+
+import re
 
 
 class UserBase(BaseModel):
     name: str = Field(None, example='Hikaru Kawasaki')
     username: str = Field(..., example='hikaru')
 
+    @validator('name')
+    @classmethod
+    def validate_name(cls, value: str) -> str:
+        name = re.sub(r'\s+', ' ', value)
+
+        if len(name) > 30:
+            raise ValueError('Name is too long')
+        return name
+
+    @validator('username')
+    @classmethod
+    def validate_username(cls, value: str) -> str:
+        if len(value) < 4:
+            raise ValueError('Username too short')
+        if len(value) > 16:
+            raise ValueError('Username too long')
+        if not re.match(r'^[a-z0-9]+$', value):
+            raise ValueError('Only a-z, 0-9 allowed in username')
+
+        return value
+
 
 class UserCreate(UserBase):
     password: str = Field(..., example='password')
+
+    @validator('password')
+    @classmethod
+    def validate_password(cls, value: str) -> str:
+        if len(value) < 8:
+            raise ValueError('Password too short')
+        return value
 
 
 class User(UserBase):
