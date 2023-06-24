0c56e274d90cbf97c95c970db7ca0bcb4a2ded30
Mehrzad Asri <mehrzad.asri@protonmail.ch>
2023-06-24 20:53:00 +0330

    Use volume for storing database

--- backend/chatter/__main__.py
+++ backend/chatter/__main__.py
@@ -4,14 +4,17 @@ import uvicorn
 
 from chatter.config import Config
 
-host = os.environ.get('CHATTER_IP', '0.0.0.0')
-port = os.environ.get('CHATTER_PORT', '8000')
-
 try:
     Config.JWT_KEY = os.environ['CHATTER_SECRET']
 except KeyError:
     print('CHATTER_SECRET environment variable is not defined. You can generate a key using `openssl rand -hex 32`')
     exit(1)
+
+db_dir = os.environ.get('CHATTER_DB_DIR', '.')
+Config.DATABASE_URL = f'sqlite:///{db_dir}/chatterdb.sqlite3'
+
+host = os.environ.get('CHATTER_IP', '0.0.0.0')
+port = os.environ.get('CHATTER_PORT', '8000')
 
 uvicorn.run(
     "chatter.main:app",
--- backend/chatter/config.py
+++ backend/chatter/config.py
@@ -2,7 +2,6 @@ import os
 
 
 class Config:
-    DATABASE_URL = 'sqlite:///./chatterdb.sqlite3'
     API_PREFIX = '/api'
 
     JWT_KEY = None
